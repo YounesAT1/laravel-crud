9f0fd1e73416445d9fa54b29782b74648f611d60
YounesAT1 <youness.monkyde@gmail.com>
2024-02-21 18:39:03 +0100

    fix the picture and cv upload

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -16,14 +16,14 @@
     <div class="container mx-auto">
       <nav class="bg-gray-100 p-4 mb-6 rounded-b-md">
         <div class="flex justify-between items-center">
-          <a href="/" class="text-slate-700 text-xl font-bold">Chi logo</a>
+          <a href="/" class="text-slate-900 text-xl font-bold">Chi logo</a>
           <div class="flex space-x-4">
             <a href="{{ route('projects.index') }}"
-              class="text-slate-700 font-semibold {{ request()->is('projects*') ? 'text-violet-700' : '' }}">Projects</a>
+              class="text-slate-700 font-semibold {{ request()->is('projects*') ? 'text-violet-700  font-bold' : '' }}">Projects</a>
             <a href="{{ route('developers.index') }}"
-              class="text-slate-700 font-semibold {{ request()->is('developers*') ? 'text-violet-700' : '' }}">Developers</a>
+              class="text-slate-700 font-semibold {{ request()->is('developers*') ? 'text-violet-700 font-bold' : '' }}">Developers</a>
             <a href="{{ route('tasks.index') }}"
-              class="text-slate-700 font-semibold {{ request()->is('tasks*') ? 'text-violet-700' : '' }}">Tasks</a>
+              class="text-slate-700 font-semibold {{ request()->is('tasks*') ? 'text-violet-700 font-bold' : '' }}">Tasks</a>
           </div>
         </div>
       </nav>
